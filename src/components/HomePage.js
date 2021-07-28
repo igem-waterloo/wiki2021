e9b0a34b6ec8ad011ba2302f649df1aebf95d619
haerutea <tszya2020@student.cis.edu.hk>
2021-07-28 19:47:11 -0400

    adding temp home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TeamPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hi, this is the home page!</p>
+            </div>
+        )
+    }
+}
+
+export default TeamPage;
