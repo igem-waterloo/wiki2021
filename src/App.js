2ae54bd78e52ca58d23eef201e269ebcbbbea1a2
Harshit Manchanda <hmanchanda27@gmail.com>
2021-09-23 19:08:30 -0400

    structure for member's page, header and footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TeamPage from "./components/TeamPage.js";
-import HomePage from "./components/HomePage.js";
-import './App.css';
+import TeamPage from "./pages/TeamPage";
+import HomePage from "./pages/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header"
+import styles from './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render() {
         return (
+            <div className="App" style={{background: '#23242D'}}>
             <Router>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                <Header />
                 <Switch>
                     <Route path="/">
                         <HomePage />
                     </Route>
-                    <Route path="/Team">
+                    <Route path="/team">
                         <TeamPage />
                     </Route>
                 </Switch>
+                </div>
+                <div>
+                <Footer />
+                </div>
             </Router>
+            </div>
           );
     }
 }
