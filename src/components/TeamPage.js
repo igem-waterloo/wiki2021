cea06d1a46ffac075c25c7f05940474af0e8dd7f
haerutea <tszya2020@student.cis.edu.hk>
2021-09-27 19:50:20 -0400

    adding random text to test position

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import Accessibility from '../toolbar/Accessibility';
 
 class TeamPage extends Component {
     render() {
         return (
-            <div>
-                <p>Hi, this will be the team page!</p>
+            <div >
+                <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rhoncus, nisi blandit rutrum venenatis, felis libero vehicula eros, efficitur rutrum est est nec leo. Vestibulum a enim est. Pellentesque lacinia ligula mi, sit amet efficitur turpis semper sed. Duis ac mauris ullamcorper, molestie enim at, sodales leo. Ut placerat non quam et vulputate. Vivamus quis maximus quam. Proin eu pellentesque sapien. Sed sagittis velit in nibh lobortis tristique. Nam semper, turpis at maximus rhoncus, mi nibh finibus nulla, ut sodales tortor dui consequat nisl. Donec sollicitudin turpis ut congue fermentum.
+
+Praesent quis sem ac orci ultrices pharetra. Phasellus commodo iaculis auctor. In et nibh eget tellus venenatis dictum eget in odio. Aenean et elit quis lacus lobortis aliquam. Morbi nibh leo, vehicula at luctus id, commodo a nunc. Fusce ut purus luctus sem tincidunt placerat. Morbi pellentesque dui quis mauris convallis, vel ornare lorem rhoncus. Pellentesque elementum, lectus nec tristique feugiat, neque dolor iaculis urna, non aliquam lectus enim vitae odio. Aliquam in ligula ultrices, fermentum ante at, scelerisque odio. Etiam vitae dictum lacus, eget posuere odio.
+
+Maecenas interdum, nisl id tincidunt convallis, nisi turpis bibendum augue, quis rhoncus ante tellus non justo. Sed eu mattis odio. Suspendisse in ante ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non consequat enim, sit amet scelerisque dui. In sit amet feugiat mi, viverra vehicula nisi. Praesent erat turpis, posuere vitae condimentum sed, faucibus eget nisi. Nullam molestie purus non imperdiet eleifend. Phasellus ac commodo ligula, et fermentum felis.
+
+Duis a ultricies risus. Aliquam erat volutpat. Praesent placerat fermentum consectetur. Etiam sodales bibendum nulla, nec vehicula ante condimentum sed. Praesent efficitur elit sit amet scelerisque pretium. Donec laoreet lacus posuere faucibus mollis. Vivamus eu orci in neque tincidunt placerat. Nulla a sapien pharetra, rutrum diam vitae, finibus dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut laoreet ipsum nisl, ut iaculis est venenatis vitae. Morbi felis nisl, sagittis dignissim urna ac, sollicitudin sagittis felis. Nam in ex ac dui pretium malesuada. Ut eget elit augue. Proin gravida massa efficitur, molestie nunc quis, faucibus leo. Nam non justo nunc.
+
+Morbi tincidunt maximus viverra. Etiam eget sem sit amet nibh ultrices aliquam vel id justo. Aliquam tempus, lorem a cursus lobortis, dolor elit tincidunt turpis, congue condimentum lorem nibh et dolor. Cras et nunc sit amet nibh congue bibendum. Etiam sodales erat sit amet orci viverra, eget tincidunt nunc tempor. Vivamus nec mollis lectus. Mauris ut ullamcorper sem. Aenean ornare risus sed nibh fermentum molestie. Aenean sit amet erat nunc. Vestibulum at tortor ullamcorper, tincidunt purus vitae, tincidunt nisl. Sed tincidunt augue eget eleifend ultrices.
+
+Pellentesque feugiat neque sed congue bibendum. Nulla hendrerit tincidunt rhoncus. Fusce nec hendrerit elit. Fusce volutpat nisl lectus, vel vestibulum tellus pellentesque et. Nunc at tristique nibh. Vivamus arcu lorem, mollis ac posuere id, imperdiet a diam. Praesent consequat tempor dui eu accumsan. Donec luctus quam ut sapien laoreet consectetur. Nam laoreet mauris ligula, quis varius mi semper vitae.
+
+Nulla aliquet, sem a auctor commodo, magna risus blandit justo, et tristique sapien tortor laoreet felis. Aenean ante orci, iaculis nec felis dapibus, cursus molestie nisl. Sed sodales, lacus a iaculis elementum, erat nisl mollis velit, ac venenatis orci dui sollicitudin turpis. Quisque malesuada id tortor sit amet maximus. Pellentesque mi elit, euismod eget turpis et, lobortis rhoncus dui. Aenean porta justo convallis mi pulvinar pretium. Fusce sodales felis vitae fringilla suscipit. Vivamus vel dignissim nunc, eu vulputate mauris. Nullam et nisi consectetur nibh pretium efficitur a at tortor. Curabitur tempus accumsan tortor, id sodales turpis euismod vitae. Aliquam lacinia, nulla vel condimentum elementum, orci est dignissim elit, ac viverra tortor mi eget sem. Cras euismod justo vitae consequat tempor. Proin convallis ante at mauris fermentum vestibulum. Ut tellus nibh, egestas vel semper a, feugiat a justo. Aliquam ultricies fringilla aliquet. Aenean ut eleifend purus.
+
+Aliquam sed accumsan nulla. Etiam est sapien, molestie in sodales vitae, pretium eget nibh. Curabitur non neque eget nisl pretium ultrices rutrum non nisl. Sed mollis tristique erat eu tristique. Morbi convallis euismod libero, ut gravida ligula mollis ut. Mauris fringilla mauris blandit sapien egestas pharetra. Vivamus eu felis ultrices, ultrices felis eget, sollicitudin lacus. Ut pretium ligula sit amet imperdiet tincidunt.
+
+Sed fermentum massa mauris. Suspendisse potenti. Nullam tristique iaculis justo at aliquam. Etiam vel justo purus. Aenean urna nisi, gravida ut dui sed, auctor rhoncus nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut blandit congue diam, at dapibus odio sollicitudin non. Sed sit amet hendrerit erat, vel ullamcorper ipsum. Sed rhoncus consectetur sapien, vitae pretium metus. Cras aliquet maximus ex, nec luctus sapien semper quis. Proin ut nibh dui. Sed convallis elit lorem, eget commodo dolor auctor ut. Aliquam erat volutpat. Vivamus euismod, turpis vitae consequat ullamcorper, mi leo luctus odio, et feugiat risus nunc et ipsum. Aenean sed justo a ante feugiat feugiat.
+
+Praesent a libero in nibh tempus mollis. Praesent faucibus eros neque, quis lacinia ex scelerisque et. Sed ante metus, tempus sed justo vitae, facilisis lobortis orci. Cras tincidunt lectus at massa dignissim dictum eleifend at ipsum. Curabitur non nibh tempor, pellentesque diam non, vehicula tellus. Donec dolor enim, imperdiet in molestie ac, fringilla a diam. Maecenas sed mattis mi. Integer id libero dictum, porta tellus faucibus, rhoncus dolor. In faucibus auctor justo non aliquam. Ut fringilla eros at lectus bibendum ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris fringilla mauris erat, vitae luctus ex sodales non. Ut vulputate, felis tempus bibendum imperdiet, turpis metus consequat nulla, nec mattis eros lacus et libero. Quisque ultrices justo ac odio rhoncus, sed faucibus lacus porta. </p>
+                <Accessibility></Accessibility>
             </div>
         )
     }
