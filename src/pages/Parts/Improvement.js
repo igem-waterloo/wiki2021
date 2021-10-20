0d7084f24fb3a3cf9b1653c54e64c3617c9411ba
haerutea <tszya2020@student.cis.edu.hk>
2021-10-20 02:06:55 -0400

    adding parts and contributions

--- src/pages/Parts/Improvement.js
+++ src/pages/Parts/Improvement.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import overallStyles from "../overall.module.scss";
-import styles from "./improvements.module.scss";
 
 class Improvement extends Component {
     render() {
