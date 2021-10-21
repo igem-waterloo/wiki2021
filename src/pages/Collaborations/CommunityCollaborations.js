975ad4af92d33ebe60d34834a742fa225f81f91e
haerutea <tszya2020@student.cis.edu.hk>
2021-10-21 01:37:07 -0400

    adding collaboration and education

---
+++ src/pages/Collaborations/CommunityCollaborations.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import overallStyles from "../overall.module.scss";
+
+class CommunityCollaborations extends Component {
+    render() {
+        return (
+            <div className={overallStyles.container}>
+                <div className={overallStyles.text_div}>
+                    <span className={overallStyles.text_heading}>
+                        Community Collaborations
+                    </span>
+                    <div className={overallStyles.subheading}>
+                        High School Outreach
+                    </div>
+                    <div className={overallStyles.description}>
+                        Between May to June, Waterloo iGEM reached out to a multitude of high schools in the city of Waterloo for the opportunity to introduce the students to the field of synthetic biology. To our delight, we received responses from Sir John A. MacDonald Secondary School’s Grade 11 Biology class as well as Cameron Heights Collegiate Institute’s Science Club! Our presentation focused on the story of insulin, focusing not only on the success and advantages of bacterial insulin expression in the production of medicine for diabetes, but also the pitfalls when it came to the lack of regulations and testing of the bacterially produced drug. In this way, we were able to introduce the students to the three major faces of synthetic biology, as incorporated into iGEM today: Lab and Design, Math and Modelling, and Human Practices. The students asked insightful questions about the field of synthetic biology (especially with regard to biotechnology!), and we were both thrilled and humbled at their receptiveness to our presentation! We are very thankful for the science departments of SJAM secondary school and CHISC for collaborating with us and allowing us to share our passion for synthetic biology with their students. 
+                    </div>
+                </div>
+                <div className={overallStyles.figure_div}>
+                    <img src="https://2021.igem.org/wiki/images/1/1d/T--Waterloo--CollaborationsHighSchool.png" alt="Collaboration with High School"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CommunityCollaborations;
--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -1,8 +1,11 @@
 
 import React, { useState } from 'react';
 import overallStyles from '../overall.module.scss';
+import GreaterWaterlooCommunity from './GreaterWaterlooCommunity';
+import IGEMCommunity from './IGEMCommunity';
+import UWaterlooCommunity from './UWaterlooCommunity';
 
-const tabs = ['Tab 1', 'Tab 2', 'References'];
+const tabs = ["Education Within iGEM Community", "Education Within University of Waterloo Community", "Education Within Greater Waterloo Community"];
 
 const Education = () => {
         const [active, setActive] = useState(tabs[0]);
@@ -18,16 +21,13 @@ const Education = () => {
                 <div className={overallStyles.text_div}>
                     <span className={overallStyles.text_heading}>Overview</span>
                     <div className={overallStyles.description}>
-                        For many people, having an extra $2,000 dollars in their pocket could mean paying for school tuition, 2 months worth of rent, a laptop,or a flight to Asia. To an individual with possible underlying ADHD symptoms, this could mean covering the cost of an initial clinical assessment, as a first step towards receiving an official diagnosis.
-                    </div>
-                    <div className={overallStyles.description}>
-                        To a working individual earning minimum wage in Ontario, this is equivalent to 140 hours of time at work before tax. Unfortunately, the cost of an ADHD assessment may lead to some individuals having no option but to opt out of the assessment due to affordability issues and financial strain(Centre for ADHD Awareness Canada [CADDAC], 2013). This potential financial hurdle can result in further under-diagnoses of both children and adults with ADHD, preventing them from receiving the treatment they need (CADDAC, 2013). As such, the design process of neuroDetech required us to consider and assess factors such as the financial impact on patients, lab manufacturing, and governmental assistance.
+                        Due to the effects of the global COVID-19 pandemic, many of the ways in which iGEM teams educate the community at large have been hindered. However, we decided to take the continuing pandemic restrictions in stride and to shift our focus to the use of virtual tools to successfully hit our Education goals. As has been outlined in our Communication and Collaborations pages, we took part in a variety of initiatives that aimed to further education within the iGEM community, within our university community and within our greater community as well. 
                     </div>
                 </div>
                 <div className={overallStyles.sections_div}>
                     {tabs.map(tab => (
                         <div key={tab} active={active === tab} onClick={() => setActive(tab)}>
-                            <div className={overallStyles.sections}>
+                            <div className={overallStyles.sections} style={{height: "max-content"}}>
                             <div className={overallStyles.section_img}></div>
                             <div className={overallStyles.section_text}>
                                 {tab}
@@ -37,15 +37,16 @@ const Education = () => {
                     ))}
                 </div>
                 <div>
-                    {/* { active === tabs[0] && (
-                        <PotentialCost/>
+                    { active === tabs[0] && (
+                        <IGEMCommunity/>
                     )}
                     { active === tabs[1] && (
-                        <ManufacturingAndLegal/>
+                        <UWaterlooCommunity/>
                     )}
+                    
                     { active === tabs[2] && (
-                        <References/>
-                    )} */}
+                        <GreaterWaterlooCommunity/>
+                    )}
                 </div>
             </div>
         )
