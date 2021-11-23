c1e025ace598a49b9ac08c41a8d10c7d7e84ebe2
haerutea <tszya2020@student.cis.edu.hk>
2021-11-23 14:40:08 -0500

    removing unused imports

--- src/pages/TeamPage/Members.js
+++ src/pages/TeamPage/Members.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import styles from "./teampage.module.scss";
 import overallStyles from '../overall.module.scss';
-import Collapsible from "../../utils/Collapsible";
 import HumanPractices from './HumanPractices';
 import LabDesign from './LabDesign';
 import MathModelling from './MathModelling';
